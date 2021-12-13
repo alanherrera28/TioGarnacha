9f065410dac93f35045d8a15763e8d0934d159ae
Alan Herrera <alanherrera.2828@gmail.com>
2021-12-13 01:14:51 -0600

    Cache Strategy y Audio Recorder

--- sw.js
+++ sw.js
@@ -1,34 +1,35 @@
 ;
 //Nombre y Versión de Cache
-const CACHE_NAME = 'v1_cache_tio_garnacha'
+const CACHE_STATIC_NAME = 'cache_tio_garnacha_v1';
+const CACHE_DYNAMIC_NAME = "cache_tio_garnacha_dinamyc_v1";
+var cacheFiles = [
+    './',
+    './vendor/fontawesome-free/css/all.min.css',
+    'https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700',
+    'https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic',
+    'vendor/magnific-popup/magnific-popup.css',
+    './css/creative.css',
+    './css/creative.min.css',
+    './vendor/jquery/jquery.min.js',
+    './vendor/bootstrap/js/bootstrap.bundle.min.js',
+    './vendor/jquery-easing/jquery.easing.min.js',
+    './vendor/magnific-popup/jquery.magnific-popup.min.js',
+    './js/creative.js',
+    './js/funciones_buttons.js',
+    'https://kit.fontawesome.com/801633f78a.js',
+    './js/startSW.js',
+    './img/TioGarna.png',
+    './img/bg-masthead.jpg',
+    './img/icons/icon512.png'
+]
 
 //durante la fase de instalación, generalmente se almacena en caché los activos estáticos
 self.addEventListener('install', e => {
+    console.log('[SW] Installed');
     e.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => {
-            return cache.addAll(
-                    [
-                        './',
-                        './vendor/fontawesome-free/css/all.min.css',
-                        'https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700',
-                        'https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic',
-                        'vendor/magnific-popup/magnific-popup.css',
-                        './css/creative.css',
-                        './css/creative.min.css',
-                        './vendor/jquery/jquery.min.js',
-                        './vendor/bootstrap/js/bootstrap.bundle.min.js',
-                        './vendor/jquery-easing/jquery.easing.min.js',
-                        './vendor/magnific-popup/jquery.magnific-popup.min.js',
-                        './js/creative.js',
-                        './js/funciones_buttons.js',
-                        'https://kit.fontawesome.com/801633f78a.js',
-                        './js/startSW.js',
-                        './img/TioGarna.png',
-                        './img/bg-masthead.jpg',
-                        './img/icons/icon512.png'
-                    ]
-                )
+        caches.open(CACHE_STATIC_NAME).then(cache => {
+            console.log('[SW] Caching cacheFiles');
+            return cache.addAll(cacheFiles)
                 .then(() => self.skipWaiting())
         })
         .catch(err => console.log('Falló registro de cache', err))
@@ -37,7 +38,7 @@ self.addEventListener('install', e => {
 
 //una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
 self.addEventListener('activate', e => {
-    const cacheWhitelist = [CACHE_NAME]
+    const cacheWhitelist = [CACHE_STATIC_NAME]
 
     e.waitUntil(
         caches.keys()
@@ -54,18 +55,26 @@ self.addEventListener('activate', e => {
     )
 })
 
-//cuando el navegador recupera una url
-self.addEventListener('fetch', e => {
-    //Responder ya sea con el objeto en caché o continuar y buscar la url real
-    e.respondWith(
-        caches.match(e.request)
-        .then(res => {
-            if (res) {
-                //recupera del cache
-                return res
+//Stale while revalidate
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then((cacheResponse) => {
+            if (cacheResponse) {
+                fetch(event.request).then((networkResponse) => {
+                    return caches.open(CACHE_STATIC_NAME).then((cache) => {
+                        cache.put(event.request, networkResponse.clone());
+                        return networkResponse;
+                    })
+                });
+                return cacheResponse;
+            } else {
+                return fetch(event.request).then((networkResponse) => {
+                    return caches.open(CACHE_STATIC_NAME).then((cache) => {
+                        cache.put(event.request, networkResponse.clone());
+                        return networkResponse;
+                    })
+                });
             }
-            //recupera la petición a la url
-            return fetch(e.request)
         })
-    )
+    );
 })
