4e85fbbfdd1b22f3c9ba7d43c49f46ee2b2b8cd2
Alan Herrera <alanherrera.2828@gmail.com>
2021-12-13 01:37:58 -0600

    Notificaciones Push

--- js/startSW.js
+++ js/startSW.js
@@ -3,3 +3,16 @@ if ('serviceWorker' in navigator) {
         .then(reg => console.log("Registro de SW Exitoso", reg))
         .catch(err => console.log("Error al tratar de registrar SW", err))
 }
+
+if (window.Notification) {
+    Notification.requestPermission().then(estatus => {
+        console.log(estatus);
+        if (estatus == "granted") {
+            let notificacion = new Notification("Tio Garnacha Te Saluda", {
+                body: "Nos encanta que hayas activado las notificacions, esperamos poder ayudarte a encontrar la garnacha perfecta.",
+                icon: "./img/icons/icon256.png",
+                image: "./img/bg-masthead.jpg"
+            });
+        }
+    })
+}
